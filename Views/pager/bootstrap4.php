89fa70b67bb1757dd40221aedd77af17df4f2243
basic-app <support@basic-app.com>
2019-01-16 11:02:43 +0300

    n/a

--- Views/pager/bootstrap4.php
+++ Views/pager/bootstrap4.php
@@ -1,27 +1,38 @@
-<?php $pager->setSurroundCount(3);?>
+<?php
+
+$config = config('Pager');
+
+if (property_exists($config, 'surroundCount'))
+{
+	$surroundCount = $config->surroundCount;
+}
+else
+{
+	$surroundCount = 3;
+}
+
+?>
+
+<?php $pager->setSurroundCount($surroundCount);?>
 
 <nav aria-label="<?= lang('Pager.pageNavigation');?>">
+
   <ul class="pagination justify-content-center">
 
-  	<?php
-  	/*
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">Previous</a>
-    </li>
-    */
-    ?>
-
 	<?php if ($pager->hasPrevious()) : ?>
+
 		<li class="page-item">
 			<a class="page-link" href="<?= $pager->getFirst();?>" aria-label="<?= lang('Pager.first');?>">
 				<span aria-hidden="true"><?= lang('Pager.first');?></span>
 			</a>
 		</li>
+		
 		<li class="page-item">
 			<a class="page-link" href="<?= $pager->getPrevious();?>" aria-label="<?= lang('Pager.previous');?>">
 				<span aria-hidden="true">&laquo;</span>
 			</a>
 		</li>
+	
 	<?php endif;?>
 
 	<?php foreach ($pager->links() as $link) : ?>
@@ -39,17 +50,21 @@
 	<?php endforeach;?>
 
 	<?php if ($pager->hasNext()) : ?>
+	
 		<li class="page-item">
 			<a class="page-link" href="<?= $pager->getNext();?>" aria-label="<?= lang('Pager.next');?>">
 				<span aria-hidden="true">&raquo;</span>
 			</a>
 		</li>
+	
 		<li class="page-item">
 			<a class="page-link" href="<?= $pager->getLast();?>" aria-label="<?= lang('Pager.last');?>">
 				<span aria-hidden="true"><?= lang('Pager.last');?></span>
 			</a>
 		</li>
+	
 	<?php endif ?>	
 
   </ul>
+
 </nav>
