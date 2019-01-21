b345b3a4917f58032b59911a409711cd1c803ccd
basic-app <support@basic-app.com>
2019-01-21 15:46:01 +0300

    n/a

--- BaseAdminFilter.php
+++ BaseAdminFilter.php
@@ -11,37 +11,59 @@ use CodeIgniter\Security\Exceptions\SecurityException;
 abstract class BaseAdminFilter implements \CodeIgniter\Filters\FilterInterface
 {
 
+	protected $loginUrl = 'admin/login';
+
+	protected function currentUserId()
+	{
+		return service('session')->get('user_id');
+	}
+
+	protected function checkAccess($user, $uri = false)
+	{
+    	if ($user->user_admin)
+    	{
+    		return true;
+    	}
+
+    	return false;
+	}
+
+	protected function getUser($id)
+	{
+		return (new UserModel)->find($user_id);
+	}
+
     public function before(RequestInterface $request)
     {
     	$uri = $request->uri->getPath();
 
-    	if ($uri == 'admin/login')
+    	if ($uri == $this->loginUrl)
     	{
     		return;
     	}
 
     	$user = null;
 
-		$user_id = service('session')->get('user_id');
+		$user_id = $this->currentUserId();
 
 		if ($user_id)
 		{
-			$user = (new UserModel)->find($user_id);
+			$user = $this->getUser($user_id);
 		}
 
 		if ($user)
 		{
-	    	if ($user->user_admin)
-	    	{
-	    		return;
-	    	}
+			if (static::checkAccess($user, $uri))
+			{
+				return;
+			}
 
 			throw SecurityException::forDisallowedAction();
 		}
 
 		helper(['url']);
 
-    	$url = site_url('admin/login');
+    	$url = site_url($this->adminUrl);
 
     	return Services::response()->redirect($url);
     }
