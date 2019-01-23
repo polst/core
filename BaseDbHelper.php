1039e439e47778ce1ed098985c2d2e82d2a5138e
basic-app <support@basic-app.com>
2019-01-23 15:36:55 +0300

    n/a

---
+++ BaseDbHelper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package Basic App Core
+ * @license MIT License
+ * @link    http://basic-app.com
+ */
+namespace BasicApp;
+
+use Config\Database;
+
+abstract class BaseDbHelper
+{
+
+    public static function now()
+    {
+        $db = Database::connect();
+
+        $query = $db->query('SELECT NOW() as now');
+
+        $row = $query->getRow();
+
+        return $row->now;
+    }
+
+}
