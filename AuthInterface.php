81cfebed6294be42b90503e08409780b0bed336e
basic-app <support@basic-app.com>
2019-02-01 23:25:14 +0300

    n/a

--- AuthInterface.php
+++ AuthInterface.php
@@ -6,6 +6,6 @@
  */
 namespace BasicApp\Core;
 
-abstract class CliController extends BaseCliController
+interface AuthInterface extends BaseAuthInterface
 {
 }
---
+++ BaseAuthInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Basic App Core
+ * @license MIT License
+ * @link    http://basic-app.com
+ */
+namespace BasicApp\Core;
+
+interface BaseAuthInterface
+{
+
+    public static function getCurrentUserId();
+
+    public static function getCurrentUser();
+
+    public static function checkPassword(string $password, string $hash);
+
+    public static function encodePassword(string $password);
+
+    public static function login($user, $expire = null);
+
+    public static function logout();
+
+}
