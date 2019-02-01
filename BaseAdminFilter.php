fe8e8fe4d109ac8b9dc524829cbee139565b10d4
basic-app <support@basic-app.com>
2019-02-02 01:01:15 +0300

    n/a

--- BaseAdminFilter.php
+++ BaseAdminFilter.php
@@ -6,55 +6,14 @@
  */
 namespace BasicApp\Core;
 
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use Config\Services;
-use CodeIgniter\Security\Exceptions\SecurityException;
 use BasicApp\Admin\Models\AdminModel;
 
-abstract class BaseAdminFilter implements \CodeIgniter\Filters\FilterInterface
+abstract class BaseAdminFilter extends AuthFilter
 {
 
-	public static function adminModelClass()
-	{
-		return AdminModel::class;
-	}
-
-    public function before(RequestInterface $request)
+    public static function getAuthModelClass()
     {
-    	$class = static::adminModelClass();
-
-    	$currentUrl = $request->uri->getPath();
-
-    	$loginUrl = $class::adminLoginUrl();
-
-    	if ($currentUrl == $loginUrl)
-    	{
-    		return;
-    	}
-
-    	$user = $class::currentAdmin();
-
-		if ($user)
-		{
-			if ($class::checkAdminAccess($user, $currentUrl))
-			{
-				return;
-			}
-
-			throw SecurityException::forDisallowedAction();
-		}
-
-		helper(['url']);
-
-    	$url = site_url($loginUrl);
-
-    	return Services::response()->redirect($url);
-    }
-
-    public function after(RequestInterface $request, ResponseInterface $response)
-    {
-        // Do something here
+        return AdminModel::class;
     }
 
 }
---
+++ BaseAuthFilter.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package Basic App Core
+ * @license MIT License
+ * @link    http://basic-app.com
+ */
+namespace BasicApp\Core;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Config\Services;
+use CodeIgniter\Security\Exceptions\SecurityException;
+use BasicApp\User\Models\UserModel;
+
+abstract class BaseAuthFilter implements \CodeIgniter\Filters\FilterInterface
+{
+
+	public static function getAuthModelClass()
+	{
+		return UserModel::class;
+	}
+
+    public function before(RequestInterface $request)
+    {
+    	$class = static::getAuthModelClass();
+
+    	$currentUrl = $request->uri->getPath();
+
+    	$loginUrl = $class::getLoginUrl();
+
+    	if ($currentUrl == $loginUrl)
+    	{
+    		return;
+    	}
+
+    	$user = $class::getCurrentUser();
+
+		if ($user)
+		{
+			throw SecurityException::forDisallowedAction();
+		}
+
+		helper(['url']);
+
+    	$url = site_url($loginUrl);
+
+    	return Services::response()->redirect($url);
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response)
+    {
+        // Do something here
+    }
+
+}
